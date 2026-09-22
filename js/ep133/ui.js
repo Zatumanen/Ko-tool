dbdc34fba00441eac9968af8031913a09cfa7e1e
Zatumanen <zatumanen123@gmail.com>
2026-09-23 01:25:34 +0300

    fix: restore EP auto-connect entrypoint

--- js/ep133/ui.js
+++ js/ep133/ui.js
@@ -1,4 +1,4 @@
-import{isConnected,onConnectionChange,onFileEvent,listDeviceFiles,getFile,getFileMetadata,uploadSampleToSlot,deleteFile,startPlayback,normalizeFileName}from './index.js';
+import{connectEp133,isConnected,onConnectionChange,onFileEvent,listDeviceFiles,getFile,getFileMetadata,uploadSampleToSlot,deleteFile,startPlayback,normalizeFileName}from './index.js';
 import{prepareEp133Sample}from './audio.js?v=20260923-1';
 import{createSampleSlots,createSampleMemory}from './sampleMemory.js';
 import{outputFileName}from '../output-name.js';
