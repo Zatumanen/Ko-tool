fb4cecc10ade9a16c7b75e38f07cf6df1bdef7e2
Zatumanen <zatumanen123@gmail.com>
2026-09-23 01:10:23 +0300

    fix: expose EP file event listener

--- js/ep133/index.js
+++ js/ep133/index.js
@@ -1,2 +1,2 @@
-export{connectEp133,disconnectEp133,isConnected,getMidiPorts,onConnectionChange}from './device.js';
+export{connectEp133,disconnectEp133,isConnected,getMidiPorts,onConnectionChange,onFileEvent}from './device.js';
 export{listDeviceFiles,getFile,getFileMetadata,uploadSampleToSlot,deleteFile,startPlayback,normalizeFileName,uploadProjectArchive,downloadProjectArchive}from './filesystem.js';
