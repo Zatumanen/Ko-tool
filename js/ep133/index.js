a181f471e5e844ebc5833f2d590facfeda1dd92a
Zatumanen <zatumanen123@gmail.com>
2026-09-22 07:13:15 +0300

    refactor: expose EP-133 UI filesystem API through index

--- js/ep133/index.js
+++ js/ep133/index.js
@@ -1,2 +1,2 @@
 export{connectEp133,disconnectEp133,isConnected,getMidiPorts,onConnectionChange}from './device.js';
-export{listDeviceFiles,getFile,deleteFile,uploadProjectArchive,downloadProjectArchive}from './filesystem.js';
+export{listDeviceFiles,getFile,getFileMetadata,uploadSampleToSlot,deleteFile,startPlayback,normalizeFileName,uploadProjectArchive,downloadProjectArchive}from './filesystem.js';
